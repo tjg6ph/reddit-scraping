d31acc3d78e3f5d6fc1ec86394012ab51aaf74e4
Tanner <tjg6ph@gmail.com>
2020-02-04 14:43:43 -0500

    Experimenting with comments

--- scraper.py
+++ scraper.py
@@ -10,6 +10,33 @@ reddit = praw.Reddit(client_id=secret.client_id, \
                      password=secret.password)
                      
 subreddit = reddit.subreddit('Vaporwave')
-top_list = subreddit.top(limit=500)
-test_list = list(top_list)
-test_list[0].comments.list()[0].body
+
+topics_dict = { "title":[], \
+                "score":[], \
+                "id":[], "url":[], \
+                "comms_num": [], \
+                "created": [], \
+                "body":[]}
+
+for submission in subreddit.top(limit=1):
+    topics_dict["title"].append(submission.title)
+    topics_dict["score"].append(submission.score)
+    topics_dict["id"].append(submission.id)
+    topics_dict["url"].append(submission.url)
+    topics_dict["comms_num"].append(submission.num_comments)
+    topics_dict["created"].append(submission.created)
+    topics_dict["body"].append(submission.selftext)
+        
+comments_body = ['']
+submission.comments.replace_more(limit=None)
+for comment in submission.comments.list():
+    comments_body.append(comment.body)
+
+topics_data = pd.DataFrame(topics_dict)
+comments_data = pd.DataFrame(comments_body)
+def get_date(created):
+    return dt.datetime.fromtimestamp(created)
+_timestamp = topics_data["created"].apply(get_date)
+topics_data = topics_data.assign(timestamp = _timestamp)
+topics_data.to_csv('vaporscrape_v1.csv', index=False)
+comments_data.to_csv('vaporcomments_v1.csv', index=False)
