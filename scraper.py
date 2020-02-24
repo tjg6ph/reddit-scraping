26333a4d7b4e83fdf0ab252fbceab5532fe96c81
Tanner <tjg6ph@gmail.com>
2020-02-24 14:25:27 -0500

    Updated scrapers

--- scraper.py
+++ scraper.py
@@ -18,7 +18,7 @@ topics_dict = { "title":[], \
                 "created": [], \
                 "body":[]}
 
-for submission in subreddit.top(limit=1):
+for submission in subreddit.top(limit=5):
     topics_dict["title"].append(submission.title)
     topics_dict["score"].append(submission.score)
     topics_dict["id"].append(submission.id)
@@ -27,16 +27,9 @@ for submission in subreddit.top(limit=1):
     topics_dict["created"].append(submission.created)
     topics_dict["body"].append(submission.selftext)
         
-comments_body = ['']
-submission.comments.replace_more(limit=None)
-for comment in submission.comments.list():
-    comments_body.append(comment.body)
-
 topics_data = pd.DataFrame(topics_dict)
-comments_data = pd.DataFrame(comments_body)
 def get_date(created):
     return dt.datetime.fromtimestamp(created)
 _timestamp = topics_data["created"].apply(get_date)
 topics_data = topics_data.assign(timestamp = _timestamp)
 topics_data.to_csv('vaporscrape_v1.csv', index=False)
-comments_data.to_csv('vaporcomments_v1.csv', index=False)
