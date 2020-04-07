efceff1b21abdb15332dc4c0b65c481974d797f2
Tanner <tjg6ph@gmail.com>
2020-04-07 15:36:56 -0400

    Completed initial post amassing

--- scraper.py
+++ scraper.py
@@ -18,7 +18,7 @@ topics_dict = { "title":[], \
                 "created": [], \
                 "body":[]}
 
-for submission in reddit.subreddit(subreddit).submissions(start=1341633600, end=1357016399):
+for submission in reddit.subreddit(subreddit).top:
     topics_dict["title"].append(submission.title)
     topics_dict["score"].append(submission.score)
     topics_dict["id"].append(submission.id)
--- scraper_pushshift.py
+++ scraper_pushshift.py
@@ -3,37 +3,78 @@ import pandas as pd
 import requests
 import json
 import csv
-
-# http://api.pushshift.io/reddit/search/submission/?subreddit=vaporwave&before=1356998400&size=500 #
+import datetime
 
 def getPushshiftData(after, before, sub):
     url = 'https://api.pushshift.io/reddit/search/submission/?size=1000&after='+str(after)+'&before='+str(before)+'&subreddit='+str(sub)
+    print(url)
     r = requests.get(url)
     data = json.loads(r.text)
     return data['data']
 
-#list of post ID's
-post_ids = []
+def collectSubData(subm):
+    subData = list() #list to store data points
+    title = subm['title']
+    url = subm['url']
+    try:
+        flair = subm['link_flair_text']
+    except KeyError:
+        flair = "NaN"    
+    author = subm['author']
+    sub_id = subm['id']
+    score = subm['score']
+    created = datetime.datetime.fromtimestamp(subm['created_utc']) #1520561700.0
+    numComms = subm['num_comments']
+    permalink = subm['permalink']
+    try:
+        body = subm['selftext']
+    except KeyError:
+        body = "NaN"
+    
+    subData.append((sub_id,title,url,author,score,created,numComms,permalink,flair,body))
+    subStats[sub_id] = subData
+
 #Subreddit to query
-sub='vaporwave'
-# Unix timestamp of date to crawl from.
-# 2018/04/01
-after = '1575158400'
-before = '1575763199'
+sub='Vaporwave'
+#before and after dates
+before = "1577836800"
+after = "1293840000"
+subCount = 0
+subStats = {}
 
-data = getPushshiftData(after, before, sub)
-
-# Will run until all posts have been gathered 
-# from the 'after' date up until todays date
+data = getPushshiftData(after, before, sub)# Will run until all posts have been gathered 
+# from the 'after' date up until before date
 while len(data) > 0:
     for submission in data:
-        post_ids.append(submission["id"])
+        collectSubData(submission)
+        subCount+=1
     # Calls getPushshiftData() with the created date of the last submission
-    data = getPushshiftData(sub=sub, after=data[-1]['created_utc'], before=data[-1]['created_utc'])
+    print(len(data))
+    print(str(datetime.datetime.fromtimestamp(data[-1]['created_utc'])))
+    after = data[-1]['created_utc']
+    data = getPushshiftData(after, before, sub)
+    
+print(len(data))
 
-obj = {}
-obj['sub'] = sub
-obj['id'] = post_ids
-# Save to json for later use
-with open("submissions.json", "w") as jsonFile:
-    json.dump(obj, jsonFile)
+print(str(len(subStats)) + " submissions have added to list")
+print("1st entry is:")
+print(list(subStats.values())[0][0][1] + " created: " + str(list(subStats.values())[0][0][5]))
+print("Last entry is:")
+print(list(subStats.values())[-1][0][1] + " created: " + str(list(subStats.values())[-1][0][5]))
+
+def updateSubs_file():
+    upload_count = 0
+    print("input filename of submission file, please add .csv")
+    filename = input()
+    file = filename
+    with open(file, 'w', newline='', encoding='utf-8') as file: 
+        a = csv.writer(file, delimiter=',')
+        headers = ["Post ID","Title","Url","Author","Score","Publish Date","Total No. of Comments","Permalink","Flair","Body"]
+        a.writerow(headers)
+        for sub in subStats:
+            a.writerow(subStats[sub][0])
+            upload_count+=1
+            
+        print(str(upload_count) + " submissions have been uploaded")
+        
+updateSubs_file()
